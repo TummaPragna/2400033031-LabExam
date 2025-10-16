74c4bbe826913c74a282df14683ef0ed40e55be6
Tumma Pragna <221049220+TummaPragna@users.noreply.github.com>
2025-10-16 11:41:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Courses from "./components/Courses";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* Navigation Links */}
+        <nav style={{ background: "#eee", padding: "10px" }}>
+          <Link to="/" style={{ margin: "10px" }}>Home</Link>
+          <Link to="/courses" style={{ margin: "10px" }}>Courses</Link>
+          <Link to="/contact" style={{ margin: "10px" }}>Contact</Link>
+        </nav>
+
+        {/* Page Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
